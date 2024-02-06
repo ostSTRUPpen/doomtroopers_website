278945ea52e2710bfbcd2b5fcadcc46116ddb8ee
VV <vojvol@post.cz>
2024-02-06 21:46:38 +0100

    Added gh-pages functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
-<script>
-	import { base } from '$app/paths';
+<script lang="ts">
 </script>
 
-<a href="{base}/about">About</a>
+<div class="py-10 px-5 print:hidden">
+	<p>
+		Vytvořil <a
+			href="https://www.linkedin.com/in/vojt%C4%9Bch-vold%C5%99ich-955577277/"
+			rel="author"
+			target="_blank"
+			class="link-hover link-primary">Vojtěch Voldřich</a
+		>
+		<span class="text-primary">2024A</span> v roce 2023.
+	</p>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess({})],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
@@ -13,7 +13,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '//TODO' : ''
+			//TODO nastavit base - pokud bude potřeba
+			base: process.env.NODE_ENV === 'production' ? '/doomtroopers_website' : ''
 		}
 	}
 };
