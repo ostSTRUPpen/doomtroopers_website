cbe34dc047500f2cea7ddb25036a4f5d2b42565d
Ost <vojvol@post.cz>
2024-02-06 22:02:08 +0100

    Testing

---
+++ src/lib/AboutMe.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let displayAboutMe: boolean = true
+</script>
+
+{#if displayAboutMe}
+<div class="py-10 px-5 print:hidden">
+	<p>
+		Správce webu (a člen týmu) <a
+			href="https://www.linkedin.com/in/vojt%C4%9Bch-vold%C5%99ich-955577277/"
+			rel="author"
+			target="_blank"
+			class="link-hover link-primary">Vojtěch Voldřich</a
+		> <span class="text-secondary">(šnek)</span>
+	</p>
+</div>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,5 @@
 <script lang="ts">
+    import AboutMe from "$lib/AboutMe.svelte";
 </script>
 
-<div class="py-10 px-5 print:hidden">
-	<p>
-		Vytvořil <a
-			href="https://www.linkedin.com/in/vojt%C4%9Bch-vold%C5%99ich-955577277/"
-			rel="author"
-			target="_blank"
-			class="link-hover link-primary">Vojtěch Voldřich</a
-		>
-		<span class="text-primary">2024A</span> v roce 2023.
-	</p>
-</div>
+<AboutMe displayAboutMe={true}/>
