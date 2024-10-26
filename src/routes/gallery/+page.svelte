2b558b538753377af181eed6066a2c3b537e50e3
ostSTRUPpen <vojvol@post.cz>
2024-10-26 23:40:27 +0200

    Added better galery types selector (images, responsibility)

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,46 +1,9 @@
+<script>
+	import GalleryTypesDisplay from "$lib/elements/GalleryTypesDisplay.svelte";
 
-<div class="ml-5 h-fit w-fit max-w-7xl rounded-lg bg-base-200 p-5">
+</script>
+<div class="ml-5 h-fit w-fit max-w-7xl rounded-lg bg-base-200 p-5 content-between">
 	<h2 class="pb-5 text-center text-5xl font-extrabold text-primary">Galerie</h2>
 	<br />
-	<div class="card card-compact w-64 sm:w-80  md:w-52 lg:w-44 xl:w-64 2xl:w-80 2xl:card-normal 3xl:w-96 bg-base-100 shadow-xl md:float-start">
-		<a href="/gallery/akce">
-			<div class="card-body">
-				<h2 class="card-title md:text-2xl 2xl:text-xl">Fotografie z akcí</h2>
-			</div>
-			<figure>
-				<img
-					src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-					alt="Shoes"
-				/>
-			</figure>
-		</a>
-	</div>
-
-	<div class="card card-compact w-64 sm:w-80 my-5 md:w-52 lg:w-44 xl:w-64 2xl:w-80 2xl:card-normal 3xl:w-96 bg-base-100 shadow-xl md:float-start md:mx-5 lg:mx-10 md:my-0">
-		<a href="/gallery/treninky">
-			<div class="card-body">
-				<h2 class="card-title md:text-2xl 2xl:text-xl">Fotografie z tréninků</h2>
-			</div>
-			<figure>
-				<img
-					src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-					alt="Shoes"
-				/>
-			</figure>
-		</a>
-	</div>
-
-	<div class="card card-compact w-64 sm:w-80 md:w-52 lg:w-44 xl:w-64 2xl:w-80 2xl:card-normal 3xl:w-96 bg-base-100 shadow-xl">
-		<a href="/gallery/propagace">
-			<div class="card-body">
-				<h2 class="card-title md:text-2xl 2xl:text-xl" >Fotografie z propagací</h2>
-			</div>
-			<figure>
-				<img
-					src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-					alt="Shoes"
-				/>
-			</figure>
-		</a>
-	</div>
+	<GalleryTypesDisplay />
 </div>
