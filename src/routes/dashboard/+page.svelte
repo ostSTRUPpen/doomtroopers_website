b64d17ef95c7fedd2c9df86f182df392e64555cf
ostSTRUPpen <vojvol@post.cz>
2024-10-26 23:41:19 +0200

    Added dashboard (just the bg and header)

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -2,4 +2,7 @@
     // TODO udělat boiler plate pro dashboard
 </script>
 
-A
+<div class="ml-5 h-fit w-fit max-w-7xl rounded-lg bg-base-200 p-5 content-between">
+	<h2 class="pb-5 text-center text-5xl font-extrabold text-primary">Nástěnka</h2>
+    <br />
+</div>
