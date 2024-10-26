b119f01038eab66c77e6dad3e8e322430eced7fa
ostSTRUPpen <vojvol@post.cz>
2024-10-26 23:45:17 +0200

    Added TODO

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,6 +14,9 @@
 	Po přesunu z free tieru přidat HTTPS přesun dat (Přes "Security+networking", "Front Door and CDN")nastavit Secure transfer required (přes "Settings" "Configuration"
 
 	// TODO možná by nebylo od věci přidat možnost zobrazit všechny akce (/all) (ona už existuje, ale není oficiální - dá se vyvolat jen ručně úpravou URL)
+	
+	// TODO vypnout možnost vybírat text a pravé tlačítko?
+	
 	*/
 </script>
 
