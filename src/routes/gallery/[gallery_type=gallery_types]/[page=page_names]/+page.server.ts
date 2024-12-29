0c8df1ac65bd4ffded937e1f066ccb15e815f44f
ostSTRUPpen <vojvol@post.cz>
2024-12-29 18:03:32 +0100

    Reformating

--- src/routes/gallery/[gallery_type=gallery_types]/[page=page_names]/+page.server.ts
+++ src/routes/gallery/[gallery_type=gallery_types]/[page=page_names]/+page.server.ts
@@ -2,35 +2,31 @@ import { AZURE_CONNECTION_STRING } from '$env/static/private';
 
 import { gallery_pages } from '$lib/data/gallery_texts';
 
-import {
-    BlobServiceClient,
-  } from '@azure/storage-blob';
+import { BlobServiceClient } from '@azure/storage-blob';
 
-  
-  
-  const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_CONNECTION_STRING)
-    
+const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_CONNECTION_STRING)
 
-  async function getCorrectImageNames(pageName: string): Promise<string[]> {
-    const containerClient = blobServiceClient.getContainerClient('dt-images');
-    const correctImages = []
-    let blobs = await containerClient.listBlobsFlat();
-    for await (const blob of blobs) {
-        const blobPath = blob.name.split("/");
-        if (blobPath[0] === pageName) {
-           correctImages.push(blob.name)
-        }
-    }
-    return correctImages;
-  }
 
-export const load = async ({ setHeaders,params }) => {
-  setHeaders({
+async function getCorrectImageNames(pageName: string): Promise<string[]> {
+	const containerClient = blobServiceClient.getContainerClient('dt-images');
+	const correctImages = []
+	let blobs = await containerClient.listBlobsFlat();
+	for await (const blob of blobs) {
+		const blobPath = blob.name.split("/");
+		if (blobPath[0] === pageName) {
+			correctImages.push(blob.name)
+		}
+	}
+	return correctImages;
+}
+
+export const load = async ({ setHeaders, params }) => {
+	setHeaders({
 		'Cache-Control': `max-age=${360}, s-maxage=${360}`
 	});
-  const pageName = params.page;
+	const pageName = params.page;
 
-  const data = gallery_pages.filter(p => p.page_name === pageName)[0]
+	const data = gallery_pages.filter(p => p.page_name === pageName)[0]
 
-  return {pageData: data, pageImages: await getCorrectImageNames(pageName)};
+	return { pageData: data, pageImages: await getCorrectImageNames(pageName) };
 }
