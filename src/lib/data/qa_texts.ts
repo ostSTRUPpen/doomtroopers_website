faf369095245b3e9662c4f065b0240f359b85b4e
ostSTRUPpen <vojvol@post.cz>
2025-09-09 21:47:35 +0200

    Added requested changes

--- src/lib/data/qa_texts.ts
+++ src/lib/data/qa_texts.ts
@@ -2,15 +2,15 @@ export const qaaData: Array<any> = [
     {
         header: "Airsoft",
         data: [
-            { // TODO revize
+            { // TODO revize gramatiky
                 q: "O čem je airsoft?",
-                a: "Airsoft je týmová taktická hra nebo sport, kde hráči používají realistické repliky zbraní střílející malé plastové kuličky. Cílem je simulovat bojové situace s důrazem na fair play, disciplínu a spolupráci mezi hráči.",
+                a: "Airsoft je týmová taktická hra nebo sport, kde hráči používají realistické repliky zbraní střílející malé plastové bio kuličky. Cílem je simulovat bojové situace s důrazem na fair play, disciplínu a spolupráci mezi hráči.",
             },
-            { // TODO revize
+            { // TODO revize gramatiky
                 q: "Co udělat když bych si chtěl zahrát?",
                 a: "Pokud si chceš zahrát s námi, stačí nás kontaktovat jako skupinu (například přes email) nebo kontaktovat jakéhokoliv člena týmu (například přes Facebook).",
             },
-            { // TODO revize
+            { // TODO revize gramatiky
                 q: "Od kolika let můžu hrát a potřebuji zbrojní průkaz?",
                 a: "V Česku můžete airsoft hrát od 18 let, protože airsoftové zbraně jsou klasifikovány jako zbraně kategorie D, které může vlastnit každá svéprávná osoba od 18 let bez nutnosti zbrojního průkazu. Většina akcí povoluje za určitých podmínek, účast už od 15 let. Pro aktuální informace je vždy potřeba nahlédnout do příslušného zákoníku a na pravidla konkrétní akce."
             },
@@ -19,23 +19,23 @@ export const qaaData: Array<any> = [
     {
         header: "KVH Doomtroopers, z.s.",
         data: [
-            { // TODO revize
+            { // TODO revize gramatiky
                 q: "Kdo jste?",
-                a: "Jsme airsoft tým z&nbsp;Havlíčkova Brodu a&nbsp;okolí a&nbsp;působíme jako občanské sdružení KVH&nbsp;&&nbsp;AS Doomtroopers z.s. od roku 2004. Jezdíme na velké i malé akce, také se zabýváme propagací airsoftu mezi širokou veřejností. Náš největší problém je volný čas, ale je s námi sranda a nepotřebujeme si na nikom honit ego."
+                a: "Jsme airsoft tým z&nbsp;Havlíčkova Brodu a&nbsp;okolí a&nbsp;působíme jako občanské sdružení KVH&nbsp;&&nbsp;AS Doomtroopers z.s. od roku 2004. Jezdíme na velké i malé akce, také se zabýváme propagací airsoftu mezi širokou veřejností."
             },
-            { // TODO revize
+            { // TODO revize gramatiky
                 q: "Jak se mohu stát členem?",
                 a: "Pro členství stačí pouze s námi vyrazit předtím na pár akcí (pro lepší seznámení) a poté už jen vyplnit přihlášku a dotazníky."
             },
-            { // TODO revize
+            { // TODO revize gramatiky
                 q: "Co pro mě členství znamená?",
-                a: "Vstup do pohodového kolektivu a možnost účastnit se větších akcí s přátelským zázemím."
+                a: "Vstup do pohodového kolektivu a možnost účastnit se větších akcí s přátelským zázemím. Možnost tréninku a výcviku, seznámení s taktikou, střelby, pohyb, maskování a další dovednosti."
             },
-            { // TODO revize
+            { // TODO revize gramatiky
                 q: "Jak často hrajete?",
                 a: "Snažíme se hrát co nejčastěji (co nám volný čas a akce dovolí)."
             },
-            { // TODO revize
+            { // TODO revize gramatiky
                 q: "Organizujete nějaké akce?",
                 a: "Organizace akcí s příběhem či nějakou myšlenkou nestíháme, ale účastníme se propagačních akcí a dětských dnů. Samozřejmě se snažíme i občas nějakou alespoň menší akci uspořádat (hlavně pro nováčky a zájemce), ale jinak se hlavně účastníme akcí pořádaných jinými týmy."
             },
