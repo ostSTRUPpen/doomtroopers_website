2cc9106f1d69d17d77df1473043fbc9db83008e2
ostSTRUPpen <vojvol@post.cz>
2025-09-11 21:00:23 +0200

    Added q&a

--- src/lib/data/qa_texts.ts
+++ src/lib/data/qa_texts.ts
@@ -1,44 +1,52 @@
+/**
+ * Soubor obsahující data pro q&A
+ * 
+ * Soubor: qa_texts.ts
+ */
+
 export const qaaData: Array<any> = [
     {
         header: "Airsoft",
         data: [
-            { // TODO revize gramatiky
-                q: "O čem je airsoft?",
-                a: "Airsoft je týmová taktická hra nebo sport, kde hráči používají realistické repliky zbraní střílející malé plastové bio kuličky. Cílem je simulovat bojové situace s důrazem na fair play, disciplínu a spolupráci mezi hráči.",
+            {
+                q: "O&nbsp;čem je airsoft?",
+                a: "Airsoft je týmová taktická hra nebo sport, kde hráči používají realistické repliky zbraní střílející malé plastové bio kuličky. Cílem je simulovat bojové situace s&nbsp;důrazem na fair play, disciplínu a&nbsp;spolupráci mezi hráči.",
             },
-            { // TODO revize gramatiky
-                q: "Co udělat když bych si chtěl zahrát?",
-                a: "Pokud si chceš zahrát s námi, stačí nás kontaktovat jako skupinu (například přes email) nebo kontaktovat jakéhokoliv člena týmu (například přes Facebook).",
+            {
+                q: "Co udělat, když bych si chtěl zahrát?",
+                a: "Pokud si chceš zahrát s&nbsp;námi, stačí nás kontaktovat jako skupinu (například přes e-mail) nebo kontaktovat jakéhokoliv člena týmu (například přes Facebook).",
             },
-            { // TODO revize gramatiky
-                q: "Od kolika let můžu hrát a potřebuji zbrojní průkaz?",
-                a: "V Česku můžete airsoft hrát od 18 let, protože airsoftové zbraně jsou klasifikovány jako zbraně kategorie D, které může vlastnit každá svéprávná osoba od 18 let bez nutnosti zbrojního průkazu. Většina akcí povoluje za určitých podmínek, účast už od 15 let. Pro aktuální informace je vždy potřeba nahlédnout do příslušného zákoníku a na pravidla konkrétní akce."
+            {
+                q: "Od kolika let můžu hrát a&nbsp;potřebuji zbrojní průkaz?",
+                a: "V&nbsp;Česku můžete airsoft hrát od 18 let, protože airsoftové zbraně jsou klasifikovány jako zbraně kategorie&nbsp;D, které může vlastnit každá svéprávná osoba od 18 let bez nutnosti zbrojního průkazu. Většina akcí povoluje za určitých podmínek účast už od 15 let. Pro aktuální informace je vždy potřeba nahlédnout do příslušného zákoníku a&nbsp;na pravidla konkrétní akce."
             },
         ]
     },
     {
         header: "KVH Doomtroopers, z.s.",
         data: [
-            { // TODO revize gramatiky
+            {
                 q: "Kdo jste?",
-                a: "Jsme airsoft tým z&nbsp;Havlíčkova Brodu a&nbsp;okolí a&nbsp;působíme jako občanské sdružení KVH&nbsp;&&nbsp;AS Doomtroopers z.s. od roku 2004. Jezdíme na velké i malé akce, také se zabýváme propagací airsoftu mezi širokou veřejností."
+                a: "Jsme airsoft tým z&nbsp;Havlíčkova Brodu a&nbsp;okolí a&nbsp;působíme jako občanské sdružení KVH &&nbsp;AS Doomtroopers z.s. od roku 2004. Jezdíme na velké i&nbsp;malé akce, také se zabýváme propagací airsoftu mezi širokou veřejností."
             },
-            { // TODO revize gramatiky
+            {
                 q: "Jak se mohu stát členem?",
-                a: "Pro členství stačí pouze s námi vyrazit předtím na pár akcí (pro lepší seznámení) a poté už jen vyplnit přihlášku a dotazníky."
+                a: "Pro členství stačí pouze s&nbsp;námi vyrazit předtím na pár akcí (pro lepší seznámení) a&nbsp;poté už jen vyplnit přihlášku a&nbsp;dotazník."
             },
-            { // TODO revize gramatiky
+            {
                 q: "Co pro mě členství znamená?",
-                a: "Vstup do pohodového kolektivu a možnost účastnit se větších akcí s přátelským zázemím. Možnost tréninku a výcviku, seznámení s taktikou, střelby, pohyb, maskování a další dovednosti."
+                a: "Vstup do pohodového kolektivu a&nbsp;možnost účastnit se větších akcí s&nbsp;přátelským zázemím. Možnost tréninku a&nbsp;výcviku, seznámení s&nbsp;taktikou, střelbou, pohybem, maskováním a&nbsp;dalšími dovednostmi."
             },
-            { // TODO revize gramatiky
+            {
                 q: "Jak často hrajete?",
-                a: "Snažíme se hrát co nejčastěji (co nám volný čas a akce dovolí)."
+                a: "Snažíme se hrát co nejčastěji (co nám volný čas a&nbsp;akce dovolí)."
             },
-            { // TODO revize gramatiky
+            {
                 q: "Organizujete nějaké akce?",
-                a: "Organizace akcí s příběhem či nějakou myšlenkou nestíháme, ale účastníme se propagačních akcí a dětských dnů. Samozřejmě se snažíme i občas nějakou alespoň menší akci uspořádat (hlavně pro nováčky a zájemce), ale jinak se hlavně účastníme akcí pořádaných jinými týmy."
+                a: "Organizace akcí s&nbsp;příběhem či nějakou myšlenkou nestíháme, ale účastníme se propagačních akcí a&nbsp;dětských dnů. Samozřejmě se snažíme i&nbsp;občas nějakou alespoň menší akci uspořádat (hlavně pro nováčky a&nbsp;zájemce), ale jinak se hlavně účastníme akcí pořádaných jinými týmy."
             },
         ]
     }
 ];
+
+/*** Konec souboru qa_texts.ts */
