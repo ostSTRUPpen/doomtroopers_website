12e76823d97353cc040890c06721effe2f0fdcdb
ostSTRUPpen <vojvol@post.cz>
2025-09-14 13:39:03 +0200

    Updated dashboard Motorsvet2025 event info

--- src/lib/data/dashboard_texts.ts
+++ src/lib/data/dashboard_texts.ts
@@ -27,20 +27,21 @@ export const other_info: dashboardTextsData[] = [];
 /**
  * Proběhlé události
  */
-export const happened_events: dashboardTextsData[] = []
-
-/**
- * Nadcházející události
- */
-export const upcoming_events: dashboardTextsData[] = [
-    {
+export const happened_events: dashboardTextsData[] = [
+      {
         heading: "Motorsvět 2025",
         display_date: "13. 9. 2025",
         day: 13,
         month: 9,
         year: 2025,
-        main_text: "Více informací na <a class='link' target='_blank' href='https://www.besip-kv.cz/ponehodova-pece/'>www.besip-kv.cz/ponehodova-pece/</a>",
+        main_text: "Více informací na <a class='link' target='_blank' href='https://www.besip-kv.cz/ponehodova-pece/'>www.besip-kv.cz/ponehodova-pece/</a> <br/><br/> <img src='https://dtimagestorage.blob.core.windows.net/dt-images/dashboard/Motosvet2025.jpg'/> ",
     },
+]
+
+/**
+ * Nadcházející události
+ */
+export const upcoming_events: dashboardTextsData[] = [
 ]
 
 /**
