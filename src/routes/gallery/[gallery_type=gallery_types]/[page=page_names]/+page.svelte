a2c13540a7414ee237fbea8fad418ea77281f437
ostSTRUPpen <vojvol@post.cz>
2025-10-04 17:57:39 +0200

    Fixed images from previous gallery being visible while new one is loading

--- src/routes/gallery/[gallery_type=gallery_types]/[page=page_names]/+page.svelte
+++ src/routes/gallery/[gallery_type=gallery_types]/[page=page_names]/+page.svelte
@@ -4,25 +4,31 @@
 -->
 
 <script lang="ts">
+	import { page } from '$app/state';
 	import Gallery from '$lib/elements/Gallery.svelte';
 
 	let { data } = $props<{ pageData: GalleryTextsDataValue; pageImages: string[] }>();
 
 	let { pageData, pageImages } = $derived(data);
+
+	// Slouží pro explicitní znovu načtení stránky při změně galerie (brání pozůstatkům obrázků z předchozí galerie)
+	let pageName = $derived(page.params.page);
 </script>
 
 {#if pageData}
 	<div class="ml-5 max-w-7xl select-none">
-		<h2 class="pb-5 text-center text-3xl font-extrabold text-primary">
+		<h2 class="text-primary pb-5 text-center text-3xl font-extrabold">
 			{@html pageData.title}, ({@html pageData.display_date.replaceAll(' ', '&nbsp;')})
 		</h2>
 		{#if pageData.main_text}
-			<article class="mb-4 rounded-box bg-base-200 p-4">{@html pageData.main_text}</article>
+			<article class="rounded-box bg-base-200 mb-4 p-4">{@html pageData.main_text}</article>
 		{/if}
 		{#if pageImages.length > 0}
-			<Gallery imageLinks={pageImages} />
+			{#key pageName}
+				<Gallery imageLinks={pageImages} />
+			{/key}
 		{:else}
-			<div class="content-between rounded-box bg-base-200 p-4 text-center">
+			<div class="rounded-box bg-base-200 content-between p-4 text-center">
 				<p class="text-error sm:text-xl sm:font-bold">
 					V galerii nebyly nalezeny žádné fotografie!
 				</p>
